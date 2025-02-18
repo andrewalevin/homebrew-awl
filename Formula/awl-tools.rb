f2e6500fe6a2b11b4d3a4bd97c1f968ef7707248
Andrew Levin <andrewlevingit@hotmail.com>
2025-02-18 06:19:31 +0300

    2025-02-18 06:19:30 N

--- Formula/awl-tools.rb
+++ Formula/awl-tools.rb
@@ -3,7 +3,7 @@ class AwlTools < Formula
   homepage "https://github.com/andrewalevin/homebrew-awl"
   url "https://raw.githubusercontent.com/andrewalevin/homebrew-awl/main/scripts/awl-tools.sh"
   version "1.0.0"
-  sha256 "<SHA256_CHECKSUM>"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   def install
     bin.install "awl-tools.sh" => "awl-tools"
