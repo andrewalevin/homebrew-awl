4a36bdb15ecd5f9febead0e97bed8f32cf90a4e3
Andrew Levin <andrewlevingit@hotmail.com>
2025-02-18 06:18:50 +0300

    2025-02-18 06:18:49 N

--- Formula/awl-git.rb
+++ Formula/awl-git.rb
@@ -1,7 +1,7 @@
 class AwlGit < Formula
   desc "Git auto-commit and push with timestamp"
   homepage "https://github.com/andrewalevin/homebrew-awl"
-  url "https://raw.githubusercontent.com/andrewalevin/homebrew-awl/main/awl-git.sh"
+  url "https://raw.githubusercontent.com/andrewalevin/homebrew-awl/main/scripts/awl-git.sh"
   version "1.0.0"
   sha256 "e443d9c6c666f983f8155334efb87af2ea7b9f4559f63cd5ae40b665f5af5f29"
 
--- Formula/awl-tools.rb
+++ Formula/awl-tools.rb
@@ -1,7 +1,7 @@
 class AwlTools < Formula
   desc "Additional CLI tools"
-  homepage "https://github.com/YOUR_GITHUB_USERNAME/homebrew-awl"
-  url "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/homebrew-awl/main/scripts/awl-tools.sh"
+  homepage "https://github.com/andrewalevin/homebrew-awl"
+  url "https://raw.githubusercontent.com/andrewalevin/homebrew-awl/main/scripts/awl-tools.sh"
   version "1.0.0"
   sha256 "<SHA256_CHECKSUM>"
 
